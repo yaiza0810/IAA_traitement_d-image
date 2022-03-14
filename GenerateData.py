0547c6b6ab72fa3f9e1c34e489d062018559bce6
NinoDauvier <80319874+NinoDauvier@users.noreply.github.com>
2022-03-14 09:54:56 +0100

    Add files via upload

--- GenerateData.py
+++ GenerateData.py
@@ -1,12 +1,14 @@
-import matplotlib as mat
-import os, os.path
-
-import numpy as np
-from PIL import Image
-import matplotlib.pyplot as plt
-from skimage.feature import hog
-from skimage import data, exposure
+import os
+import os.path
 import random as rd
+
+import matplotlib as mat
+import numpy as np
+import skimage
+from PIL import Image
+from skimage import data
+from skimage.feature import hog
+
 
 def rd_insert(imgs, y, desc, classe):
     rand = rd.randrange(2)
@@ -28,287 +30,107 @@ def resize(file):
         imResize.save('Data/SmallResized/' + file + '/' + f + 'SmallResized.jpg', 'JPEG', quality=95)
 
 
-def gen_data_all():
-    imgs = []
+def gen_descr(fct):
+    description = []
     y = []
     path = "Data/Resized/Mer"
     valid_images = [".jpg", ".jpeg"]
     for f in os.listdir(path):
         ext = os.path.splitext(f)[1]
         if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgsbis = []
-            for ligne in data:
-                for pixel in ligne:
-                    for couleur in pixel:
-                        imgsbis.append(couleur)
-            rd_insert(imgs, y, imgsbis, 1)
-
+            description.append(fct(path + "/" + f))
+            y.append(1)
     path = "Data/Resized/Ailleurs"
     for f in os.listdir(path):
         ext = os.path.splitext(f)[1]
         if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgsbis = []
-            for ligne in data:
-                for pixel in ligne:
-                    for couleur in pixel:
-                        imgsbis.append(couleur)
-            rd_insert(imgs, y, imgsbis, 0)
-
-    return imgs, y
-
-def gen_data_all_blue():
-    imgs = []
-    y = []
-    path = "Data/Resized/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgsbis = []
-            for ligne in data:
-                for pixel in ligne:
-                    # print((pixel[1]+pixel[2]+pixel[0]), "pix")
-                    if int(pixel[1])+int(pixel[2])+int(pixel[0]) == 0:
-                        couleur = 0
-                    else:
-                        couleur = int(pixel[2])/(int(pixel[1])+int(pixel[2])+int(pixel[0]))
-                    imgsbis.append(couleur)
-            rd_insert(imgs, y, imgsbis, 1)
-
-    path = "Data//Resized/Ailleurs"
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgsbis = []
-            for ligne in data:
-                for pixel in ligne:
-                    if int(pixel[1]) + int(pixel[2]) + int(pixel[0]) == 0:
-                        couleur = 0
-                    else:
-                        couleur = int(pixel[2]) / (int(pixel[1]) + int(pixel[2]) + int(pixel[0]))
-                    imgsbis.append(couleur)
-            rd_insert(imgs, y, imgsbis, 0)
-
-    return imgs, y
-
-def gen_data_hist():
-    imgs = []
-    y = []
-    path = "Data/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = Image.open(path + "/" + f)
-            imgs.append(data.histogram())
-            y.append(1)
-
-    path = "Data/Ailleurs"
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = Image.open(path + "/" + f)
-            imgs.append(data.histogram())
+            description.append(fct(path + "/" + f))
             y.append(0)
-
-    return imgs, y
-
-
-def hist_blue():  # 0.72222 avec Gauss
-    imgs = []
-    y = []
-    count = 0
-    path = "Data/Resized/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = Image.open(path + "/" + f)
-            r, g, b = data.split()
-            maximum = max(b.histogram())
-            list = []
-            for nb in b.histogram():
-                list.append(nb / maximum)
-            imgs.append(list)
-            y.append(1)
-
-    path = "Data/Resized/Ailleurs"
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = Image.open(path + "/" + f)
-            r, g, b = data.split()
-            maximum = max(b.histogram())
-            list = []
-            for nb in b.histogram():
-                list.append(nb / maximum)
-            imgs.append(list)
-            y.append(0)
-    # print(imgs)
-    return imgs, y
+    return description, y
 
 
-def gen_data_1_px():
-    imgs = []
-    y = []
-    path = "DATA/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgs.append(data[0][0])
-            y.append(1)
+def pixel_by_pixel(path):
+    data = mat.image.imread(path)
+    list_attr = []
+    for ligne in data:
+        for pixel in ligne:
+            for couleur in pixel:
+                list_attr.append(couleur)
+    return list_attr
 
-    path = "DATA/Ailleurs"
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data = mat.image.imread(path + "/" + f)
-            imgs.append(data[0][0])
-            y.append(0)
-
-    return imgs, y
-
-
-def gradient():
-    imgs = []
-    y = []
-    path = "Data/Resized/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            imgs.append(data_img)
-            y.append(1)
-
-    path = "Data/Resized/Ailleurs"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            imgs.append(data_img)
-            y.append(0)
-
-    x_gradient = []
-    for image in imgs:
-        fd = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                 cells_per_block=(1, 1), visualize=False, channel_axis=-1)
-        # print(type(fd))
-        fdbis = fd.tolist()
-
-        x_gradient.append(fdbis)
-    # print(x_gradient, type(x_gradient))
-    # print(y, len(y))
-
-    # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
-    #
-    # ax1.axis('off')
-    # ax1.imshow(image, cmap=plt.cm.gray)
-    # ax1.set_title('Input image')
-    #
-    # # Rescale histogram for better display
-    # hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 10))
-    #
-    # ax2.axis('off')
-    # ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
-    # ax2.set_title('Histogram of Oriented Gradients')
-    # plt.show()
-
-    return x_gradient, y
+def blue_proportion_by_pix(path):
+    data = mat.image.imread(path)
+    list_attr = []
+    for ligne in data:
+        for pixel in ligne:
+            if int(pixel[1]) + int(pixel[2]) + int(pixel[0]) == 0:
+                couleur = 0
+            else:
+                couleur = int(pixel[2]) / (int(pixel[1]) + int(pixel[2]) + int(pixel[0]))
+            list_attr.append(couleur)
+    return list_attr
 
 
-def gradient_blue():
-    imgs = []
-    y = []
-    path = "Data/Resized/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            imgs.append(data_img)
-            y.append(1)
+def histo(path):
+    data = Image.open(path)
+    return data.histogram()
 
-    path = "Data/Resized/Ailleurs"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            imgs.append(data_img)
-            y.append(0)
 
-    x_gradient = []
-    for image in imgs:
-        fd = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                 cells_per_block=(1, 1), visualize=False, channel_axis=-1)
-        # print(type(fd))
-        fdbis = fd.tolist()
-        for ligne in image:
-            for pixel in ligne:
-                # print((pixel[1]+pixel[2]+pixel[0]), "pix")
-                if int(pixel[1]) + int(pixel[2]) + int(pixel[0]) == 0:
-                    couleur = 0
-                else:
-                    couleur = int(pixel[2]) / (int(pixel[1]) + int(pixel[2]) + int(pixel[0]))
-            fdbis.append(couleur)
-        x_gradient.append(fdbis)
-    print(x_gradient, len(x_gradient))
-    print(y, len(y))
-    return x_gradient, y
+def hist_blue(path):  # 0.72222 avec Gauss
+    data = Image.open(path)
+    r, g, b = data.split()
+    return b.histogram()
 
-def gradient_blue_hist():
-    imgs = []
-    hist =[]
-    y = []
-    path = "Data/Resized/Mer"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            data = Image.open(path + "/" + f)
-            # print(data.histogram())
-            imgs.append(data_img)
-            hist.append(data.histogram())
-            y.append(1)
 
-    path = "Data/Resized/Ailleurs"
-    valid_images = [".jpg", ".jpeg"]
-    for f in os.listdir(path):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() in valid_images:
-            data_img = mat.image.imread(path + "/" + f)
-            data = Image.open(path + "/" + f)
-            imgs.append(data_img)
-            hist.append(data.histogram())
-            y.append(0)
+def only_1_pix(path):
+    data = mat.image.imread(path)
+    return data[0][0]
 
-    x_gradient = []
-    for image in imgs:
-        fd = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                 cells_per_block=(1, 1), visualize=False, channel_axis=-1)
-        # print(type(fd))
-        fdbis = fd.tolist()
-        for ligne in image:
-            for pixel in ligne:
-                # print((pixel[1]+pixel[2]+pixel[0]), "pix")
-                if int(pixel[1]) + int(pixel[2]) + int(pixel[0]) == 0:
-                    couleur = 0
-                else:
-                    couleur = int(pixel[2]) / (int(pixel[1]) + int(pixel[2]) + int(pixel[0]))
-            fdbis.append(couleur)
-        for img in hist:
-            for pix in img:
-                fdbis.append(pix)
-        x_gradient.append(fdbis)
-    # print(x_gradient, len(x_gradient))
-    # print(y, len(y))
-    return x_gradient, y
+
+def gradient(path):
+    data = mat.image.imread(path)
+    fd = hog(data, orientations=8, pixels_per_cell=(16, 16),
+             cells_per_block=(1, 1), visualize=False, channel_axis=-1)
+    x_gradient = fd.tolist()
+    return x_gradient
+
+
+def gradient_blue(path):
+    data = Image.open(path)
+    r, g, b = data.split()
+    data = mat.image.imread(path)
+    fd = hog(b, orientations=8, pixels_per_cell=(16, 16),
+             cells_per_block=(1, 1), visualize=False, channel_axis=-1)
+    x_gradient = fd.tolist()
+    return x_gradient
+
+
+def gradient_blue_hist(path):
+    return gradient(path) + hist_blue(path)
+
+def comatrice(path):
+    data = mat.image.imread(path)
+    data = np.array(data)
+    data_reformed = []
+    for i in range(len(data)):
+        for j in range(len(data[0])):
+            data_reformed.append([data[i][j][2], data[i][j][2], data[i][j][2]])
+    glcmimg = skimage.feature.graycomatrix(data_reformed, distances=[10], angles=[0], levels=256,
+                                           symmetric=True,
+                                           normed=True)
+    contrast = skimage.feature.graycoprops(glcmimg, 'contrast')
+    homogeneite = skimage.feature.graycoprops(glcmimg, 'homogeneity')
+    asm = skimage.feature.graycoprops(glcmimg, 'dissimilarity')
+    list_attr = []
+    for ligne in contrast:
+        for pixel in ligne:
+            list_attr.append(pixel)
+
+    for ligne in homogeneite:
+        for pixel in ligne:
+            list_attr.append(pixel)
+
+    for ligne in asm:
+        for pixel in ligne:
+            list_attr.append(pixel)
+    return list_attr
